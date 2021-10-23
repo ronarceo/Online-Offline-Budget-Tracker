9ef2eb75d4af1e307b6199a1591721083ca15ef8
ronarceo <ronaldmarceo@gmail.com>
2021-10-23 16:45:51 -0500

    added code to activate the service worker and remove old data from the cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,3 +25,20 @@ self.addEventListener("install", function (evt) {
     // has finished installing
     self.skipWaiting();
 });
+
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
