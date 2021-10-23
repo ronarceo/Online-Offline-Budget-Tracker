90059bdbd81c51d3596ccf6f33a9f1ef752c2442
ronarceo <ronaldmarceo@gmail.com>
2021-10-23 16:42:08 -0500

    added code to install and register service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,3 +13,15 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function (evt) {
+    // pre cache all static assets
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+});
