71b39d2d606c37dc9c491f05d6566c3ea9ed5276
ronarceo <ronaldmarceo@gmail.com>
2021-10-23 16:32:41 -0500

    added files to cache, cache name, and data cache name to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,15 @@
-console.log("Hello from service worker!");
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    'manifest.webmanifest',
+    '/service-worker.js',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
